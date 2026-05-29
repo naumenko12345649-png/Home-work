87d808f8f8af1bd6a44783d1e036ab5813ad16f7
Naumenko <naumenko12345649@gmail.com>
2026-05-29 20:39:45 +0300

    list sort

---
+++ main4.py
@@ -0,0 +1,54 @@
+my_list = [0, 1, 0, 12, 3]
+numbers = []
+zeros = []
+for i in my_list:
+    if i == 0:
+        zeros.append(i)
+    else:
+        numbers.append(i)
+    result = numbers + zeros
+print(result)
+
+my_list1 = [0]
+numbers = []
+zeros = []
+for i in my_list1:
+    if i == 0:
+        zeros.append(i)
+    else:
+        numbers.append(i)
+    result = numbers + zeros
+print(result)
+
+my_list1 = [1, 0, 13, 0, 0, 0, 5]
+numbers = []
+zeros = []
+for i in my_list1:
+    if i == 0:
+        zeros.append(i)
+    else:
+        numbers.append(i)
+    result = numbers + zeros
+print(result)
+
+my_list1 = [9, 0, 7, 31, 0, 45, 0, 45, 0, 0, 96, 0]
+numbers = []
+zeros = []
+for i in my_list1:
+    if i == 0:
+        zeros.append(i)
+    else:
+        numbers.append(i)
+    result = numbers + zeros
+print(result)
+
+my_list22 = []
+numbers = []
+zeros = []
+for i in my_list22:
+    if i == 0:
+        zeros.append(i)
+    else:
+        numbers.append(i)
+        result = numbers + zeros
+    print(result)
